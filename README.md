a4c52cabcc53d301da3e393eb78e6069eabce16c
asorokin <a40in@inbox.ru>
2020-11-17 18:23:13 +0200

    change chan and add example

--- README.md
+++ README.md
@@ -1,6 +1,52 @@
 # Alarm
 
-
 **Модуль "Будильник"**
 
 Пакет представляет собой простой будильник, который отправляет пустую структуру в канал в заданый момент времени, с учетом необходимой временной зоны.
+
+Пример:
+
+```
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	alarm "github.com/NuclearLouse/utilities-alarm"
+)
+
+func main() {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	timeAlarm, err := time.Parse("15:04:05", "17:58:00")
+	if err != nil {
+		log.Fatalln("set time alarm:", err)
+	}
+
+	a, err := alarm.New(ctx, timeAlarm, "Europe/Chisinau")
+	if err != nil {
+		log.Fatalln("create alarm:", err)
+	}
+LOOP:
+	for {
+		select {
+		case ta := <-a.A:
+			fmt.Printf("Будильник сработал в : %s\n", ta.Format("15:04:05"))
+			// делаю что нужно по будильнику и/или выхожу
+			ctx.Done()
+			break LOOP
+		default:
+			// тут что-то делаю до срабатывания будильника
+
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+}
+
+```
--- alarm.go
+++ alarm.go
@@ -8,59 +8,63 @@ import (
 
 // Alarm ...
 type Alarm struct {
-	A <-chan struct{}
+	A chan time.Time
 }
 
-// New возвращает структуру содержащую только одно поле A, которое является каналом
-// в который отправиться пустая структура в заданое время. Значения в канал будет посылаться
+// New возвращает структуру содержащую только одно поле A, которое является каналом и
+// в который в заданое время отправится значение текущего времени. Значения в канал будет посылаться
 // каждые сутки пока через контекст не будет вызвана отмена. Точность срабатывани до секунды.
-// На вход функция принимает контекст, время срабатывания и не обязательный параметр 
+// На вход функция принимает контекст, время срабатывания и не обязательный параметр
 // временной зоны по которой необходимо срабатывание.
-// Если временная зона не задана, по умолчанию принимается локальная зона. 
+// Если временная зона не задана, по умолчанию принимается локальная зона.
 // Время задается в формате 15:04:05, зона задается в формате IANA.
 func New(ctx context.Context, timeAlarm time.Time, location ...string) (*Alarm, error) {
-	a := make(chan struct{}, 1)
-	A := &Alarm{A: a}
 
-	timeWithDate, err := time.Parse("2006-01-02 15:04:05", fmt.Sprintf("%s %s", time.Now().Format("2006-01-02"), timeAlarm.Format("15:04:05")))
+	timeWithDate, err := time.Parse("2006-01-02 15:04:05",
+		fmt.Sprintf("%s %s",
+			time.Now().Format("2006-01-02"),
+			timeAlarm.Format("15:04:05")))
 	if err != nil {
-		return A, err
+		return nil, err
 	}
 
 	l := time.Now().Local().Location()
 	if len(location) > 0 {
 		l, err = time.LoadLocation(location[0])
 		if err != nil {
-			return A, err
+			return nil, err
 		}
 	}
 
-	timeAlarmWithLoc, err := time.ParseInLocation("2006-01-02 15:04:05", timeWithDate.Format("2006-01-02 15:04:05"), l)
+	timeAlarmWithLoc, err := time.ParseInLocation("2006-01-02 15:04:05",
+		timeWithDate.Format("2006-01-02 15:04:05"), l)
 	if err != nil {
-		return A, err
+		return nil, err
 	}
 	local := time.Now().Local().Location()
 	shedule := timeAlarmWithLoc.In(local)
 
 	ta, err := time.ParseInLocation("15:04:05", shedule.Format("15:04:05"), local)
 	if err != nil {
-		return A, err
+		return nil, err
 	}
 
-	go func(ctx context.Context, tAlarm time.Time, sigAlarm chan struct{}){
+	a := &Alarm{A: make(chan time.Time, 1)}
+	go func(ctx context.Context, tAlarm time.Time, sigAlarm chan time.Time) {
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			default:
-				tnow, _ := time.ParseInLocation("15:04:05", time.Now().Format("15:04:05"), time.Now().Local().Location())
+				tnow, _ := time.ParseInLocation("15:04:05",
+					time.Now().Format("15:04:05"), time.Now().Local().Location())
 				if tnow.Equal(tAlarm) {
-					sigAlarm <- struct{}{}
+					sigAlarm <- time.Now()
 				}
 				time.Sleep(1 * time.Second)
 			}
 		}
-	}(ctx, ta, a)
+	}(ctx, ta, a.A)
 
-	return A, nil
+	return a, nil
 }
